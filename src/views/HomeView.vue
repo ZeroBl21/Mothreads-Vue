3a664bfa1838136cec82a130d429e80835020fe5
Darwin Perdomo <ZeroBl@hotmail.com>
2023-05-13 18:17:51 -0400

    refactor(pages/HomeView): Change search input to use query params

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,25 @@
 <script setup>
 import { ref, watchEffect } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
 
-import SearchInput from '../components/SearchInput.vue'
-import BookList from '../components/BookList.vue'
+import SearchInput from '@/components/SearchInput.vue'
+import BookList from '@/components/BookList.vue'
 import useInfinityScroll from '@/composables/useInfinityScroll.js'
 import api from '@/api.js'
 
 const books = ref([])
-const q = ref('Git')
 const startIndex = ref(0)
 const loading = ref(false)
 const el = ref(null)
+
+const route = useRoute()
+const router = useRouter()
 
 useInfinityScroll(el, () => (startIndex.value += 10))
 
 watchEffect(async () => {
   loading.value = true
-  const newBooks = await api.books.list(q.value, startIndex.value)
+  const newBooks = await api.books.list(route.query?.q || "Git", startIndex.value)
   if (startIndex.value > 0) {
     books.value.push(...newBooks)
     loading.value = false
@@ -32,7 +35,7 @@ function handleSubmit(e) {
   if (!search) return
 
   startIndex.value = 0
-  q.value = search.value
+  router.replace({ query: { q: search.value } })
 }
 </script>
 
